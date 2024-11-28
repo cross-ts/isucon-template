0954556f5a27bad8fd025fab2c7a1704507a1601
cross-ts <ts.kouhei@gmail.com>
2024-11-29 04:50:44 +0900

    Lambdaとログ周りを適当に設定した

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { ProfilerStack } from '../lib/profiler-stack';
+import { LambdaFunctionLogGroupAspect } from '../lib/aspects/lambda-function-log-group-aspect';
 
 const app = new cdk.App();
-new ProfilerStack(app, 'ProfilerStack', {
-});
+const stack = new ProfilerStack(app, 'ProfilerStack', {});
+
+cdk.Aspects.of(stack).add(new LambdaFunctionLogGroupAspect());
--- cdk/lib/profiler-stack.ts
+++ cdk/lib/profiler-stack.ts
@@ -1,8 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cr from 'aws-cdk-lib/custom-resources';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class ProfilerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const crConfig = cr.CustomResourceConfig.of(this);
+    crConfig.addRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    crConfig.addLogRetentionLifetime(logs.RetentionDays.ONE_DAY);
+
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+    const fn = new lambda.Function(this, 'Function', {
+      runtime: lambda.Runtime.PYTHON_3_13,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('assets/lambda'),
+    });
+    bucket.grantReadWrite(fn);
   }
 }
