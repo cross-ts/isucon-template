9e810f22de537ca4990b666246a0fc1f965f84b9
cross-ts <ts.kouhei@gmail.com>
2024-12-01 03:08:04 +0900

    めんどくさくなった

--- cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { ProfilerStack } from '../lib/profiler-stack';
-import { LambdaFunctionLogGroupAspect } from '../lib/aspects/lambda-function-log-group-aspect';
-
-const app = new cdk.App();
-const stack = new ProfilerStack(app, 'ProfilerStack', {});
-
-cdk.Aspects.of(stack).add(new LambdaFunctionLogGroupAspect());
